199eda0161290067b831fc8e7e644d7847766a77
Sambulo Senda <admin@users-MacBook-Air.local>
2021-01-03 12:50:50 +0000

    first commit

---
+++ src/components/CountryTable/CountryTable.js
@@ -0,0 +1,87 @@
+import { StylesProvider } from "@material-ui/core";
+import {
+  KeyboardArrowDownRounded,
+  KeyboardArrowUpRounded,
+} from "@material-ui/icons";
+import React, { useState } from "react";
+import styles from "./CountryTable.module.css";
+
+const orderBy = (countries, value, direction) => {
+  if (direction === "asc") {
+    return [...countries].sort((a, b) => (a[value] > b[value] ? 1 : -1));
+  }
+
+  if (direction === "desc") {
+    return [...countries].sort((a, b) => (a[value] > b[value] ? -1 : 1));
+  }
+
+  return countries;
+};
+
+const SortArrow = ({ direction }) => {
+  if (!direction) {
+    return <> </>;
+  }
+
+  if (direction === "desc") {
+    return (
+      <div className={styles.heading__arrow}>
+        <KeyboardArrowDownRounded />
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.heading__arrow}>
+        <KeyboardArrowUpRounded />
+      </div>
+    );
+  }
+};
+
+function CountryTable({ countries }) {
+  const [direction, setdirection] = useState();
+  const [value, setvalue] = useState();
+
+  const orderedCountry = orderBy(countries, value, direction);
+
+  const switchDirection = () => {
+    if (!direction) {
+      setdirection("desc");
+    } else if (direction == "desc") {
+      setdirection("asc");
+    } else {
+      setdirection(null);
+    }
+  };
+
+  const setValueAndDirection = (value) => {
+    switchDirection()
+    setvalue(value);
+  }
+
+
+  return (
+    <div>
+      <div className={styles.heading}>
+        <button className={styles.heading_name}>
+          <div>Name</div>
+          <SortArrow />
+        </button>
+
+        <button className={styles.heading_population} onClick={() =>setValueAndDirection('population')}>
+          <div>Population</div>
+          <SortArrow direction={direction} />
+        </button>
+      </div>
+
+      {orderedCountry.map((country) => (
+        <div className={styles.row}>
+          <div className={styles.name}>{country.name}</div>
+          <div className={styles.population}>{country.population}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default CountryTable;
---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Layout from "../components/Layout/Layout";
+import SearchInput from "../components/SearchInput/SearchInput";
+import CountryTable from "../components/CountryTable/CountryTable";
+
+export default function Home({ countries }) {
+  return (
+    <Layout>
+      <div className={styles.counts}>Found {countries.length} countries</div>
+      <SearchInput placeholder="filter by name, Region" />
+      <CountryTable countries={countries} />
+    </Layout>
+  );
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+  return {
+    props: {
+      countries,
+    },
+  };
+};
